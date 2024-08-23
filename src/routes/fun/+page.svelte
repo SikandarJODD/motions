fb6c5d482f381d889f4742aff571ae7d36692580
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2024-08-23 12:44:30 +0530

    Clock Effect

--- src/routes/fun/+page.svelte
+++ src/routes/fun/+page.svelte
@@ -2,9 +2,8 @@
   import Wrapper from "$lib/components/animations/Wrapper.svelte";
   import CalendarIndev from "$lib/components/craft/CalendarIndev.svelte";
   import ImgCard from "$lib/components/craft/ImgCard.svelte";
+  import RemoteTeamClock from "$lib/components/craft/RemoteTeamClock.svelte";
 </script>
 
 <!-- <ImgCard /> -->
-<Wrapper class='bg-background'>
-  <CalendarIndev />
-</Wrapper>
+<RemoteTeamClock />
